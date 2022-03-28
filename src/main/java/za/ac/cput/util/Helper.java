faf744d3610056a3eb03ded4655170854233f7b5
Mariam <mariamgallie17@gmail.com>
2022-03-28 17:54:49 +0200

    BookingFactory, ClientFactory and Helper done by 219094837

---
+++ src/main/java/za/ac/cput/util/Helper.java
@@ -0,0 +1,12 @@
+package src.main.java.za.ac.cput.util;
+
+/*Helper.java Util for the Helper
+Author: Mariam Gallie (219094837)
+Date: 28 March 2022
+*/
+import java.util.UUID;
+
+public class Helper {
+    public static String generateId(){ return UUID.randomUUID().toString();
+    }
+}
