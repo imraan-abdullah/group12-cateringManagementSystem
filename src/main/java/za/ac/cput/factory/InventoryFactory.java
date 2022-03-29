9dd6a155fdb103bd7c1f7135f9ab0547c7ed95ae
Thina Mzosindiso Nontwabaza <219189153@mycput.ac.za>
2022-03-29 16:39:17 +0200

    Factories: 219189153

---
+++ src/main/java/za/ac/cput/factory/InventoryFactory.java
@@ -0,0 +1,24 @@
+package za.ac.cput.factory;
+import za.ac.cput.entity.Inventory;
+import src.main.java.za.ac.cput.util.Helper;
+
+/*
+ * Inventory.java
+ * Factory for the Inventory
+ * Author: Thina Mzosindiso Nontwabaza (219189153)
+ * Date: 28 March 2022.
+ */
+
+public class InventoryFactory {
+
+    //Creating inventory objects
+    public static Inventory createInventory(int itemID, String itemDescription, int itemQuantity, String stockAvailability){
+        Inventory inventory = new Inventory.Builder()
+                .setItemID(itemID)
+                .setItemDescription(itemDescription)
+                .setItemQuantity(itemQuantity)
+                .setStockAvailability(stockAvailability)
+                .build();
+        return inventory;
+    }
+}
