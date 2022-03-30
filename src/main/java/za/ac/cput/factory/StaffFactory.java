5f4535254e3a8b46a96b234a5fb33dded811353c
Velenkosini Prince Jeza <218191669@mycput.ac.za>
2022-03-30 23:20:17 +0200

    my second commit

---
+++ src/main/java/za/ac/cput/factory/StaffFactory.java
@@ -0,0 +1,36 @@
+package za.ac.cput.factory;
+
+
+import za.ac.cput.entity.Staff;
+import src.main.java.za.ac.cput.util.Helper;
+
+/* Staff.java
+ Entity for the Staff
+ Author: Velenkosini Prince Jeza (218191669)
+ Date: 29 March 2022
+*/
+
+public class StaffFactory {
+
+    public static Staff createStaff (String employeeId, String firstName, String lastName, String staffType){
+
+        if(employeeId == null || employeeId.equals("") || employeeId.matches(".*[a-zA-Z]+.*"))
+            return null;
+
+        if(firstName == null || firstName.equals(""))
+            return null;
+
+        if(lastName == null || lastName.equals(""))
+            return null;
+
+        if(staffType == null || staffType.equals(""))
+            return null;
+
+        return new Staff.Builder()
+                .setEmployeeId(employeeId)
+                .setFirstName(firstName)
+                .setLastName(lastName)
+                .setStaffType(staffType)
+                .build();
+    }
+}
