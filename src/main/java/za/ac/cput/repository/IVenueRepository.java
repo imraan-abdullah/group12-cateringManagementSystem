600c085bcffb3bfae7983ec9c93b33e86267638f
Thina Mzosindiso Nontwabaza <219189153@mycput.ac.za>
2022-08-07 23:34:36 +0200

    Repo

--- src/main/java/za/ac/cput/repository/IVenueRepository.java
+++ src/main/java/za/ac/cput/repository/IVenueRepository.java
@@ -1,4 +1,5 @@
 package za.ac.cput.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import za.ac.cput.entity.Venue;
 
 /*
@@ -6,11 +7,12 @@ import za.ac.cput.entity.Venue;
  * Interface
  * Author: Thina Mzosindiso Nontwabaza (219189153)
  * Date: 04 April 2022.
+ * Edited 05 August 2022
  */
 
 import java.util.Set;
 
-public interface IVenueRepository extends IRepository <Venue, String>{
-    public Set <Venue> getAll();
+public interface IVenueRepository extends JpaRepository<Venue, String> {
+    //public Set <Venue> getAll();
 
 }
