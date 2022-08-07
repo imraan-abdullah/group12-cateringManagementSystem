20feb7afc22487e34201c137e3e5e0b30bd2d5c3
Thina Mzosindiso Nontwabaza <219189153@mycput.ac.za>
2022-08-07 23:13:15 +0200

    Repository connection with DB

--- src/main/java/za/ac/cput/factory/VenueFactory.java
+++ src/main/java/za/ac/cput/factory/VenueFactory.java
@@ -1,5 +1,6 @@
 package za.ac.cput.factory;
 import za.ac.cput.entity.Venue;
+import za.ac.cput.util.StringHelper;
 
 
 /*
@@ -7,11 +8,22 @@ import za.ac.cput.entity.Venue;
  * Factory for the Inventory
  * Author: Thina Mzosindiso Nontwabaza (219189153)
  * Date: 28 March 2022.
+ * Edited 06 August 2022
  */
 
 public class VenueFactory {
 
-    //Creating venue objects
+    public static Venue build(String venueID, String buildingName, int numberOfSeats){
+        StringHelper.checkStringParam("venueID", venueID);
+        StringHelper.checkStringParam("buildingName", buildingName);
+        StringHelper.isNullOrNegative(numberOfSeats);
+        return new Venue.Builder()
+                .setVenueID(venueID)
+                .setBuildingName(buildingName)
+                .setNumberOfSeats(numberOfSeats)
+                .build();
+    }
+/*    //Creating venue objects
     public static Venue createVenue(String venueID, String buildingName, int numberOfSeats){
         Venue venue = new Venue.Builder()
                 .setVenueID(venueID)
@@ -19,5 +31,5 @@ public class VenueFactory {
                 .setNumberOfSeats(numberOfSeats)
                 .build();
         return venue;
-    }
+    }*/
 }
