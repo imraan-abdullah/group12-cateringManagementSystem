0faf4959aec753a5e877b906376a8a02352901e3
Ameer <220005060@mycput.ac.za>
2022-10-17 13:00:06 +0200

    Security

--- src/test/java/za/ac/cput/controller/AdminControllerTest.java
+++ src/test/java/za/ac/cput/controller/AdminControllerTest.java
@@ -1,19 +1,31 @@
+/*
+AdminControllerTest.java
+Author: Ameer Samsodien (220005060)
+Date : 9 October 2022
+*/
+
 package za.ac.cput.controller;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import za.ac.cput.entity.Admin;
 import za.ac.cput.factory.AdminFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 class AdminControllerTest {
+
+    public static String ADMIN_SECURITY_USERNAME = "admin-admin";
+    public static String ADMIN_SECURITY_PASSWORD = "12345";
+    public static String CLIENT_SECURITY_USERNAME = "admin-client";
+    public static String CLIENT_SECURITY_PASSWORD = "54321";
 
     @LocalServerPort
     private int port;
@@ -32,27 +44,39 @@ class AdminControllerTest {
         this.baseUrl = "http://localhost:" + this.port + "/cateringManagementSystem/admin/";
     }
 
+
     @Test
     @Order(1)
     void safe()
     {
         String url = baseUrl + "save";
-        ResponseEntity<Admin> response = this.restTemplate.postForEntity(url, this.admin, Admin.class);
-        System.out.println(response);
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<Admin> entity = new HttpEntity<>(admin, headers);
+        ResponseEntity<Admin> response = restTemplate
+                .withBasicAuth(ADMIN_SECURITY_USERNAME,ADMIN_SECURITY_PASSWORD)
+                .exchange(url, HttpMethod.POST, entity, Admin.class);
+
         assertAll(
                 () -> assertEquals(HttpStatus.OK,response.getStatusCode()),
                 () -> assertNotNull(response.getBody())
         );
     }
 
+
     @Test
     @Order(2)
     void read()
     {
         String url = baseUrl + "read/" + this.admin.getAdminNum();
-        ResponseEntity<Admin> response = this.restTemplate.getForEntity(url, Admin.class);
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<Admin> response = restTemplate
+                .withBasicAuth(CLIENT_SECURITY_USERNAME,CLIENT_SECURITY_PASSWORD)
+                .exchange(url, HttpMethod.GET, entity, Admin.class);
         assertAll(
-                () ->  assertEquals(HttpStatus.OK,response.getStatusCode()),
+                () -> assertEquals(HttpStatus.OK, response.getStatusCode()),
                 () -> assertNotNull(response.getBody())
         );
     }
@@ -62,7 +86,13 @@ class AdminControllerTest {
     void delete()
     {
         String url = baseUrl + "delete/" + this.admin.getAdminNum();
-        this.restTemplate.delete(url);
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<Admin> response = restTemplate
+                .withBasicAuth(ADMIN_SECURITY_USERNAME,ADMIN_SECURITY_PASSWORD)
+                .exchange(url, HttpMethod.DELETE, entity, Admin.class);
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     }
 
     @Test
@@ -70,11 +100,17 @@ class AdminControllerTest {
     void findAll()
     {
         String url = baseUrl + "all";
-        ResponseEntity<Admin[]> response = this.restTemplate.getForEntity(url, Admin[].class);
+
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = restTemplate
+                .withBasicAuth(CLIENT_SECURITY_USERNAME,CLIENT_SECURITY_PASSWORD)
+                .exchange(url, HttpMethod.GET, entity, String.class);
+
+        System.out.println("Show all");
+        System.out.println(response);
         System.out.println(response.getBody());
-        assertAll(
-                () -> assertEquals(HttpStatus.OK,response.getStatusCode()),
-                () -> assertTrue (response.getBody().length == 1)
-        );
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 }
--- src/test/java/za/ac/cput/controller/LoginControllerTest.java
+++ src/test/java/za/ac/cput/controller/LoginControllerTest.java
@@ -1,19 +1,31 @@
+/*
+LoginControllerTest.java
+Author: Ameer Samsodien (220005060)
+Date : 9 October 2022
+*/
+
 package za.ac.cput.controller;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import za.ac.cput.entity.Login;
 import za.ac.cput.factory.LoginFactory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+
 class LoginControllerTest {
+
+    public static String ADMIN_SECURITY_USERNAME = "login-admin";
+    public static String ADMIN_SECURITY_PASSWORD = "12345";
+    public static String CLIENT_SECURITY_USERNAME = "login-client";
+    public static String CLIENT_SECURITY_PASSWORD = "54321";
 
     @LocalServerPort
     private int port;
@@ -28,29 +40,41 @@ class LoginControllerTest {
     void setUp()
     {
         assertNotNull(controller);
-        this.login = LoginFactory.createLogin("16", "33", "password");
+        this.login = LoginFactory
+                .createLogin("16", "33", "password");
         this.baseUrl = "http://localhost:" + this.port + "/cateringManagementSystem/login/";
     }
+
 
     @Test
     @Order(1)
     void safe()
     {
         String url = baseUrl + "save";
-        ResponseEntity<Login> response = this.restTemplate.postForEntity(url,this.login,Login.class);
-        System.out.println(response);
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<Login> entity = new HttpEntity<>(login, headers);
+        ResponseEntity<Login>response = restTemplate
+                .withBasicAuth(ADMIN_SECURITY_USERNAME, ADMIN_SECURITY_PASSWORD)
+                .exchange(url, HttpMethod.POST, entity, Login.class);
+
         assertAll(
-                () -> assertEquals(HttpStatus.OK,response.getStatusCode()),
-                () -> assertNotNull(response.getBody())
+                ()-> assertEquals(HttpStatus.OK, response.getStatusCode()),
+                ()-> assertNotNull(response.getBody())
         );
     }
+
 
     @Test
     @Order(2)
     void read()
     {
         String url = baseUrl + "read/" + this.login.getLoginId();
-        ResponseEntity<Login> response = this.restTemplate.getForEntity(url, Login.class);
+        System.out.println(url);
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<Login> response = this.restTemplate
+                .withBasicAuth(CLIENT_SECURITY_USERNAME, CLIENT_SECURITY_PASSWORD)
+                .exchange(url, HttpMethod.GET, entity, Login.class);
         assertAll(
                 () -> assertEquals(HttpStatus.OK,response.getStatusCode()),
                 () -> assertNotNull(response.getBody())
@@ -58,21 +82,36 @@ class LoginControllerTest {
     }
 
     @Test
+    @Order(3)
     void delete()
     {
         String url = baseUrl + "delete/" + this.login.getLoginId();
-        this.restTemplate.delete(url);
+        //this.restTemplate.delete(url);
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<Login> response = restTemplate
+                .withBasicAuth(ADMIN_SECURITY_USERNAME, ADMIN_SECURITY_PASSWORD)
+                .exchange(url, HttpMethod.DELETE, entity, Login.class);
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
     }
 
     @Test
+    @Order(4)
     void findAll()
     {
         String url = baseUrl + "all";
-        ResponseEntity<Login[]> response = this.restTemplate.getForEntity(url, Login[].class);
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(null, headers);
+        ResponseEntity<String> response = this.restTemplate
+                .withBasicAuth(CLIENT_SECURITY_USERNAME, CLIENT_SECURITY_PASSWORD)
+                .exchange(url, HttpMethod.GET, entity, String.class);
+
+        System.out.println("Show all");
+        System.out.println(response);
         System.out.println(response.getBody());
+
         assertAll(
-                () -> assertEquals(HttpStatus.OK,response.getStatusCode()),
-                () -> assertTrue(response.getBody().length == 1)
+                ()-> assertEquals(HttpStatus.OK,response.getStatusCode())
         );
     }
 }
